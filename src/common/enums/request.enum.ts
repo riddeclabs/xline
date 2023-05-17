60b773ea759573081828ecfde1d90df44c425a7e
Apollo <evgeni.valentino@gmail.com>
2023-05-17 17:56:27 +0300

    [XLIN-10] Add request status enums

---
+++ src/common/enums/request.enum.ts
@@ -0,0 +1,30 @@
+export enum WithdrawRequestStatus {
+    PENDING = "PENDING",
+    FINISHED = "FINISHED",
+}
+//FIXME: add statuses
+
+export enum DepositRequestStatus {
+    PENDING = "PENDING",
+    FINISHED = "FINISHED",
+}
+//FIXME: add statuses
+
+export enum BorrowRequestStatus {
+    PENDING = "PENDING",
+    FINISHED = "FINISHED",
+}
+//FIXME: add statuses
+
+export enum RepayRequestStatus {
+    PENDING = "PENDING",
+    FINISHED = "FINISHED",
+}
+//FIXME: add statuses
+
+export enum ActionTypes {
+    DEPOSIT,
+    WITHDRAW,
+    BORROW,
+    REPAY,
+}
