f332822c6024c937c80314947526d62805cb1e5c
Apollo <evgeni.valentino@gmail.com>
2023-05-17 17:59:53 +0300

    [XLIN-10] Add api description to operator controller

--- src/modules/operators/operators.controller.ts
+++ src/modules/operators/operators.controller.ts
@@ -13,7 +13,9 @@ import { JwtAuthGuard } from "src/guards";
 
 import { CreateOperatorDto } from "./dto/create-operator.dto";
 import { OperatorsService } from "./operators.service";
+import { ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Admins")
 @Controller("admins")
 export class OperatorsController {
     constructor(private readonly operatorsService: OperatorsService) {}
