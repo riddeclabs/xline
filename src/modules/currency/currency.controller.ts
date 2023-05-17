37f1b057a1715eaad43cf74a0dc874019b9974b4
Apollo <evgeni.valentino@gmail.com>
2023-05-17 17:41:36 +0300

    [XLIN-10] Add Currency module

---
+++ src/modules/currency/currency.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { CurrencyService } from "./currency.service";
+import { ApiTags } from "@nestjs/swagger";
+import { CreateCurrencyDto } from "./dto/create-currency.dto";
+
+@ApiTags("Currency")
+@Controller("currency")
+export class CurrencyController {
+    constructor(private readonly currencyService: CurrencyService) {}
+
+    @Post("collateral")
+    createCollateralCurrency(@Body() dto: CreateCurrencyDto) {
+        return this.currencyService.createCollateralCurrency(dto);
+    }
+
+    @Get("collateral")
+    getAllCollateralCurrency() {
+        return this.currencyService.getAllCollateralCurrency();
+    }
+
+    @Get("collateral/:collateralCurrencyId")
+    getCollateralCurrencyById(@Param("collateralCurrencyId") id: string) {
+        return this.currencyService.getCollateralCurrency(+id);
+    }
+
+    @Post("debt")
+    createDebtCurrency(@Body() dto: CreateCurrencyDto) {
+        return this.currencyService.createDebtCurrency(dto);
+    }
+
+    @Get("debt")
+    getAllBorrowCurrency() {
+        return this.currencyService.getAllDebtCurrency();
+    }
+
+    @Get("debt/:debtCurrencyId")
+    getBorrowCurrencyById(@Param("debtCurrencyId") id: string) {
+        return this.currencyService.getDebtCurrency(+id);
+    }
+}
---
+++ src/modules/currency/currency.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CollateralCurrency, DebtCurrency } from "../../database/entities";
+import { Repository } from "typeorm";
+import { CreateCurrencyDto } from "./dto/create-currency.dto";
+
+@Injectable()
+export class CurrencyService {
+    constructor(
+        @InjectRepository(CollateralCurrency)
+        private collateralCurrencyRepo: Repository<CollateralCurrency>,
+        @InjectRepository(DebtCurrency) private debtCurrencyRepo: Repository<DebtCurrency>
+    ) {}
+
+    async getCollateralCurrency(collateralCurrencyId: number) {
+        return this.collateralCurrencyRepo.findOneByOrFail({ id: collateralCurrencyId });
+    }
+
+    async getDebtCurrency(debtCurrencyId: number) {
+        return this.debtCurrencyRepo.findOneBy({ id: debtCurrencyId });
+    }
+
+    async getAllCollateralCurrency() {
+        return this.collateralCurrencyRepo.find();
+    }
+
+    async getAllDebtCurrency() {
+        return this.debtCurrencyRepo.find();
+    }
+
+    async createCollateralCurrency(createCollateralCurrencyDto: CreateCurrencyDto) {
+        const currency = this.collateralCurrencyRepo.create(createCollateralCurrencyDto);
+        return this.collateralCurrencyRepo.save(currency);
+    }
+
+    async createDebtCurrency(createDebtCurrencyDto: CreateCurrencyDto) {
+        const currency = this.debtCurrencyRepo.create(createDebtCurrencyDto);
+        return this.debtCurrencyRepo.save(currency);
+    }
+}
