f81cc879e230a33ed02afe522f45a1072bb462b1
Apollo <evgeni.valentino@gmail.com>
2023-05-17 17:54:42 +0300

    [XLIN-10] Add fixed number helper fns

---
+++ src/common/fixed-number.ts
@@ -0,0 +1,109 @@
+export function parseUnits(value: string | number, decimals = 18) {
+    if (typeof value === "number") {
+        value = value.toString();
+    }
+
+    if (decimals == null) {
+        decimals = 0;
+    }
+    const multiplier = getMultiplier(decimals);
+
+    // Is it negative?
+    const negative = value.substring(0, 1) === "-";
+    if (negative) {
+        value = value.substring(1);
+    }
+
+    // Split it into a whole and fractional part
+    const comps = value.split(".");
+    if (comps.length > 2) {
+        throw new Error(`ParseUnitsBigInt: Too many decimal points. Value: ${value}`);
+    }
+
+    let whole = comps[0];
+    let fraction = comps[1];
+
+    if (!whole) {
+        whole = "0";
+    }
+    if (!fraction) {
+        fraction = "0";
+    }
+
+    // Trim trailing zeros
+    while (fraction[fraction.length - 1] === "0") {
+        fraction = fraction.substring(0, fraction.length - 1);
+    }
+    if (fraction.length > multiplier.length - 1) {
+        throw new Error("Fractional component exceeds decimals");
+    }
+
+    // If decimals is 0, we have an empty string for fraction
+    if (fraction === "") {
+        fraction = "0";
+    }
+    // Fully pad the string with zeros to get to wei
+    while (fraction.length < multiplier.length - 1) {
+        fraction += "0";
+    }
+
+    const wholeValue = BigInt(whole);
+    const fractionValue = BigInt(fraction);
+
+    let wei = wholeValue * BigInt(multiplier) + fractionValue;
+    if (negative) {
+        wei = wei * BigInt(-1);
+    }
+
+    return wei;
+}
+
+export function formatUnits(value: bigint, decimals = 18) {
+    if (decimals == null) {
+        decimals = 0;
+    }
+    const multiplier = getMultiplier(decimals);
+
+    const negative = value < 0n;
+    if (negative) {
+        value = value * BigInt(-1);
+    }
+
+    let fraction = (value % BigInt(multiplier)).toString();
+    while (fraction.length < multiplier.length - 1) {
+        fraction = "0" + fraction;
+    }
+
+    // Strip training 0
+    const match = fraction.match(/^([0-9]*[1-9]|0)(0*)/);
+
+    fraction = match && match[1] ? match[1] : "0";
+
+    const whole = (value / BigInt(multiplier)).toString();
+
+    let res: string;
+    if (multiplier.length === 1) {
+        res = whole;
+    } else {
+        res = whole + "." + fraction;
+    }
+
+    if (negative) {
+        res = "-" + res;
+    }
+
+    return res;
+}
+
+function getMultiplier(decimals: number) {
+    let zeros = "";
+    if (decimals >= 0 && decimals <= 256 && !(decimals % 1)) {
+        while (decimals > 0) {
+            zeros += "0";
+            decimals -= 1;
+        }
+        return "1" + zeros;
+    }
+
+    throw new Error(`Incorrect decimal value. decimals: ${decimals}`);
+}
--- src/common/index.ts
+++ src/common/index.ts
@@ -2,3 +2,4 @@ export * from "./enums/admin.enum";
 export * from "./middlewares/locals.middleware";
 export * from "./query-mutator";
 export * from "./pagination";
+export * from "./helper-function";
