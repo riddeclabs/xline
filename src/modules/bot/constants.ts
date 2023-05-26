a3cc2788e7e3c61cf5ac59c34ec968c05f6e93ae
Apollo <evgeni.valentino@gmail.com>
2023-05-26 18:55:10 +0300

    [XLIN-3] Update bot constants

--- src/modules/bot/constants.ts
+++ src/modules/bot/constants.ts
@@ -2,9 +2,11 @@ export enum MAIN_MENU_OPTIONS {
     TERM_AND_CONDITION = "termAndCondition",
     CURRENT_RATES = "currentRates",
     NEW_CREDIT_REQUEST = "newCreditRequest",
+    MANAGE_PORTFOLIO = "managePortfolio",
     VIEW_ACTIVE_LINE = "viewActiveLine",
     VIEW_REQUEST = "viewRequest",
     BACK_MAIN_MENU = "back",
+    CONTACT_SUPPORT = "contactSupport",
 }
 
 export enum SUPPORTED_TOKENS {
@@ -16,3 +18,8 @@ export enum SignApplicationOptions {
     APPROVE = "approve",
     DISAPPROVE = "disapprove",
 }
+
+// Text mutator constants
+
+export const LEFT_INDENT_BD_S = "🔸                         ";
+export const RIGHT_INDENT_BD_S = "                🔸";
