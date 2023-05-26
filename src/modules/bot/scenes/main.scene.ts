85baad3ec25527c5cc953e437d0e9b84b576c7e6
Ales Ananich <ales.ananich@gmail.com>
2023-05-26 14:27:56 +0300

    Add How it works button

--- src/modules/bot/scenes/main.scene.ts
+++ src/modules/bot/scenes/main.scene.ts
@@ -55,11 +55,41 @@ export class MainScene {
                         text: "🦈 View your requests",
                         callback_data: `goto:${MAIN_MENU_OPTIONS.VIEW_REQUEST}`,
                     },
+                    {
+                        text: "⚙️ How it works",
+                        callback_data: MAIN_MENU_OPTIONS.HOW_IT_WORKS,
+                    },
                 ],
                 { columns: 1 }
             )
         );
         this.botCommon.tryToSaveSceneMessage(ctx, msg);
+    }
+
+    @Action(buildTypeExp(MAIN_MENU_OPTIONS.HOW_IT_WORKS))
+    async onHowItWorks(@Ctx() ctx: MainSceneContext) {
+        await ctx.editMessageText(
+            "*Choose Your Collateral*\n" +
+                "Select Bitcoin \\(BTC\\) or Ethereum \\(ETH\\) as the collateral for your credit line\\.\n\n" +
+                "*Provide your requisites*\n" +
+                "You will be asked to provide the necessary data: your name and IBAN you want to receive money\\.\n\n" +
+                "*Set Your Collateral Amount and choose utilization*\n" +
+                "Select one of 3 initial risk strategies to utilize our collateral: LOW \\(50\\%\\), MEDIUM \\(60\\%\\), HIGH \\(80\\%\\)\n" +
+                "Remember, higher utilization means a higher risk of liquidation\\.\n\n" +
+                "*Send crypto and receive*\n" +
+                "XLine will generate and provide you a wallet address, associated with your credit line\\.\n" +
+                "After you transfer crypto to this wallet, you will receive USD, based on the utilization rate you choose\\.\n\n" +
+                "*Monitor & Manage*\n" +
+                "Keep track of your collateral value and utilization to avoid liquidation\\. Make adjustments as needed to maintain a healthy balance\\.\n\n" +
+                "You could always check the statuses of your credit line and adjustment requests in the menu\\.\n",
+                { parse_mode: "MarkdownV2"}
+        );
+
+        await ctx.editMessageReplyMarkup(
+            Markup.inlineKeyboard([this.botCommon.goBackButton()], {
+                columns: 1,
+            }).reply_markup
+        );
     }
 
     @Action(buildTypeExp(MAIN_MENU_OPTIONS.TERM_AND_CONDITION))
