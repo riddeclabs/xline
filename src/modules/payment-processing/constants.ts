6a2ff17b92eeb1563febfe6ba8bdc5b29c50e043
Apollo <evgeni.valentino@gmail.com>
2023-05-29 13:38:08 +0300

    [XLIN-37] Add enum with expected callback types

--- src/modules/payment-processing/constants.ts
+++ src/modules/payment-processing/constants.ts
@@ -1,2 +1,7 @@
 export const GET_WALLET_PATH = "/wallet/address";
 export const WITHDRAWAL_PATH = "/withdrawal";
+
+export enum CallbackTypes {
+    DEPOSIT = "DEPOSIT",
+    WITHDRAWAL = "WITHDRAWAL",
+}
