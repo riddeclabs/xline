e6b1c02b5a2d9b44621e67668f0385036567d91e
Eugene <57300277+Apollo50@users.noreply.github.com>
2023-06-16 12:09:19 +0300

    [XLIN-34]  Part 2: Update resolve repay request endpoint (#24)
    * [XLIN-34-2] Tech debt: formatter issues, fix `CreditLineDetails` related code.
    
    * [XLIN-34-2] Update query to get repay request
    
    * [XLIN-34-2] Create a separate Dto to resolve repay request
    
    * [XLIN-34-2] Update function for resolving repayRequest, add function to reject deprecated repay request
    
    * [XLIN-34-2] Update pull_request_template.md

--- src/modules/request-resolver/dto/resolve-request.dto.ts
+++ src/modules/request-resolver/dto/resolve-request.dto.ts
@@ -1,4 +1,5 @@
 import { IsNumber, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class ResolveCryptoBasedRequestDto {
     @IsString()
@@ -32,4 +33,31 @@ export class ResolveFiatBasedRequestDto {
     rawTransferAmount!: string;
     @IsString()
     status!: string;
+}
+
+export class ResolveRepayRequestDto {
+    @ApiProperty({
+        description: "ID of the request to resolve",
+        example: 1,
+    })
+    @IsNumber()
+    requestId!: number;
+    @ApiProperty({
+        description: "IBAN from which the money was received",
+        example: "AD14 0008 0001 0012 3456 7890",
+    })
+    @IsString()
+    ibanFrom!: string;
+    @ApiProperty({
+        description: "Bank account name from which the money was received",
+        example: "John Doe",
+    })
+    @IsString()
+    nameFrom!: string;
+    @ApiProperty({
+        description: "Amount that has been received",
+        example: "100.50",
+    })
+    @IsString()
+    rawTransferAmount!: string;
 }
