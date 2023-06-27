dc3773fef0ca5fde4be690073df97552065ad303
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-06-27 10:02:20 +0300

    fix: status filtering

--- src/modules/backoffice/backoffice.service.ts
+++ src/modules/backoffice/backoffice.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { CreditLineStatus, Role } from "../../common";
+import { CreditLineStatus, RepayRequestStatus, Role } from "../../common";
 import {
     BorrowRequest,
     Operator,
@@ -137,7 +137,10 @@ export class BackOfficeService {
             .leftJoinAndSelect("creditLine.debtCurrency", "debtCurrency")
             .leftJoinAndSelect("creditLine.userPaymentRequisite", "userPaymentRequisite")
             .leftJoinAndSelect("creditLine.user", "user")
-            .where("CAST(user.chat_id AS TEXT) like :chatId", { chatId: `%${chatId}%` })
+            .where("repay.repayRequestStatus = :status", {
+                status: RepayRequestStatus.VERIFICATION_PENDING,
+            })
+            .andWhere("CAST(user.chat_id AS TEXT) like :chatId", { chatId: `%${chatId}%` })
             .andWhere("creditLine.refNumber ilike  :refNumber", { refNumber: `%${refNumber}%` })
             .skip(page * PAGE_LIMIT_REQUEST)
             .take(PAGE_LIMIT_REQUEST)
