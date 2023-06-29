041ebac9ef4407a5b263ccf2febd5b8e84be8fef
Apollo <evgeni.valentino@gmail.com>
2023-06-29 09:43:10 +0300

    [XLIN-34] Add enums with XGW transaction statuses and callback types. Update `callback.dto.ts`

--- src/modules/payment-processing/constants.ts
+++ src/modules/payment-processing/constants.ts
@@ -1,14 +1,2 @@
 export const GET_WALLET_PATH = "/wallet/address";
 export const WITHDRAWAL_PATH = "/withdrawal";
-
-export enum CallbackTypes {
-    DEPOSIT = "deposit",
-    WITHDRAWAL = "withdrawal",
-}
-
-export enum CallbackTransactionStatus {
-    PROCESSING = "processing",
-    CREATED = "created",
-    CONFIRMED = "confirmed",
-    FAILED = "failed",
-}
