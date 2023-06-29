b173014a7cbb976c0d9545a781f98f3094d82a09
Apollo <evgeni.valentino@gmail.com>
2023-06-29 09:50:30 +0300

    [XLIN-34] Add validation function for withdraw scene

--- src/common/input-validation.ts
+++ src/common/input-validation.ts
@@ -1,4 +1,15 @@
 import { electronicFormatIBAN, validateIBAN, ValidateIBANResult, ValidationErrorsIBAN } from "ibantools";
+import { CollateralCurrency } from "../database/entities";
+import { parseUnits } from "./fixed-number";
+import { SUPPORTED_TOKENS } from "../modules/bot/constants";
+import { isAddress } from "ethers";
+import { validate } from "bitcoin-address-validation";
+
+export enum WithdrawSceneIncorrectInputReason {
+    INCORRECT_STRUCT_OR_ZERO,
+    INCORRECT_DECIMALS,
+    EXCEEDS_MAX_AMOUNT,
+}
 
 export function validateIban(input: string): ValidateIBANResult {
     const iban = electronicFormatIBAN(input);
@@ -9,4 +20,50 @@ export function validateIban(input: string): ValidateIBANResult {
 export function validateName(input: string): boolean {
     if (input.split(" ").length < 2) return false;
     return /^[A-Za-z\s]*$/.test(input);
+}
+
+export function isIncorrectWithdrawInputValue(
+    inputValue: string,
+    collateralCurrency: CollateralCurrency,
+    maxAllowedWithdrawAmount: bigint
+): WithdrawSceneIncorrectInputReason | null {
+    let formattedValue;
+
+    try {
+        // We use helper function `parseUnits` to validate if the struct of user input is correct.
+        // Function validate amount accuracy, format and etc
+        formattedValue = parseUnits(inputValue, collateralCurrency.decimals);
+    } catch (e: unknown) {
+        if (e instanceof Error && e.message.includes("exceeds decimals")) {
+            return WithdrawSceneIncorrectInputReason.INCORRECT_DECIMALS;
+        }
+        return WithdrawSceneIncorrectInputReason.INCORRECT_STRUCT_OR_ZERO;
+    }
+
+    if (formattedValue <= 0n) {
+        return WithdrawSceneIncorrectInputReason.INCORRECT_STRUCT_OR_ZERO;
+    }
+
+    if (formattedValue > maxAllowedWithdrawAmount) {
+        return WithdrawSceneIncorrectInputReason.EXCEEDS_MAX_AMOUNT;
+    }
+
+    return null;
+}
+
+export function validateAddressToWithdraw(inputValue: string, collateralCurrency: CollateralCurrency) {
+    let isValid = false;
+
+    switch (collateralCurrency.symbol) {
+        case SUPPORTED_TOKENS.ETH:
+            isValid = isAddress(inputValue);
+            break;
+        case SUPPORTED_TOKENS.BTC:
+            isValid = validate(inputValue);
+            break;
+        default:
+            throw new Error("Unsupported currency. Cannot validate the address");
+    }
+
+    return isValid;
 }
