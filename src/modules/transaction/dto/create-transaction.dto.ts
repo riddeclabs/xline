83248b67d29cfc63233e054285957fb6889897ef
Ales Ananich <ales.ananich@gmail.com>
2023-07-11 16:42:13 +0300

    [XLIN-61] Fix tx status on Repay resolve (#38)

--- src/modules/transaction/dto/create-transaction.dto.ts
+++ src/modules/transaction/dto/create-transaction.dto.ts
@@ -1,9 +1,20 @@
+import { IsEnum, IsIBAN, IsString } from "class-validator";
+import { FiatTransactionStatus } from "src/common";
+import { IsBigInt } from "src/decorators/class-validator-extended.decorator";
+
 export class CreateFiatTransactionDto {
     readonly borrowRequestId!: number | null;
     readonly repayRequestId!: number | null;
+    @IsIBAN()
     readonly ibanFrom!: string;
+    @IsIBAN()
     readonly ibanTo!: string;
+    @IsString()
     readonly nameFrom!: string;
+    @IsString()
     readonly nameTo!: string;
+    @IsBigInt()
     readonly rawTransferAmount!: bigint;
+    @IsEnum(FiatTransactionStatus)
+    readonly status!: FiatTransactionStatus;
 }
